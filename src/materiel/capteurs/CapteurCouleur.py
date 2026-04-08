83a320846b970cddfdf28cb08939bd387530f797
CyrilDn <cyril.denis@std.heh.be>
2026-04-08 09:24:00 +0200

    feat: adding CapteurCouleur class

---
+++ src/materiel/capteurs/CapteurCouleur.py
@@ -0,0 +1,13 @@
+class CapteurCouleur:
+    def __init__(self, adresse_i2c: str, bus_i2c: object):
+        self.adresse_i2c = adresse_i2c
+        self.bus_i2c = bus_i2c
+
+    def lire_valeurs_brutes() -> tuple:
+        pass
+
+    def normaliser_rgb() -> tuple:
+        pass
+
+    def detecter_couleur_dominante() -> str:
+        pass
