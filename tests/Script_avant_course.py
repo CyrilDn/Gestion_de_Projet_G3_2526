84411e828569fa74a6372261cfff2b00aef98750
Allan <druart.allanpro@gmail.com>
2026-04-09 11:07:35 +0200

    fix: correction des appels de fonction pour le positionnement du servo dans les tests de vérification des moteurs

--- tests/Script_avant_course.py
+++ tests/Script_avant_course.py
@@ -64,16 +64,16 @@ class ScriptAvantCourse:
         self.controleur.moteur1.arreter()
         self.controleur.moteur2.arreter()
 
-        
+
         print("Test 2.4 : Moteur - Tourner à gauche")
-        self.controleur.servo.positionner(angle=45)  # Tourner à gauche
+        self.controleur.servo.positionner(angle_brut=45)  # Tourner à gauche
         time.sleep(1)
-        self.controleur.servo.positionner(angle=90)  # Recentrer le servo
+        self.controleur.servo.positionner(angle_brut=90)  # Recentrer le servo
         time.sleep(1)
         print("Test 2.5 : Moteur - Tourner à droite")
-        self.controleur.servo.positionner(angle=135)  # Tourner à droite
+        self.controleur.servo.positionner(angle_brut=135)  # Tourner à droite
         time.sleep(1)
-        self.controleur.servo.positionner(angle=90)  # Recentrer le servo
+        self.controleur.servo.positionner(angle_brut=90)  # Recentrer le servo
 
     def _verifier_batterie(self):
         """Vérifie la batterie"""
