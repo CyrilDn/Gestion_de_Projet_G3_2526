def0788a92649c1afe1b7add3fee8481119fb103
CyrilDn <cyril.denis@std.heh.be>
2026-04-10 14:43:21 +0200

    fix: splitting movements

--- tests/demi_tour.py
+++ tests/demi_tour.py
@@ -48,47 +48,58 @@ class ScriptDemiTour:
 
             """Manœuvre 1"""
             self.controleur.servo.positionner(angle_brut = 45)  # Tourne à gauche de 45 degrés
-            time.sleep(2)  
+            time.sleep(1)  
             self.controleur.moteur1.avancer(vitesse = 40)  # Avance légèrement pour compléter le demi-tour
             self.controleur.moteur2.avancer(vitesse = 40)
-            time.sleep(2)  
+            time.sleep(1)  
             self.controleur.moteur1.arreter()  # Arrête les moteurs
             self.controleur.moteur2.arreter()
             time.sleep(1)
             self.controleur.servo.positionner(angle_brut = 90)  # Recentrer le servo
             time.sleep(1)
             self.controleur.servo.positionner(angle_brut = 135)  # Tourne à droite de 135 degrés
-            time.sleep(2)
+            time.sleep(1)
+
+            """Manœuvre 2"""
             self.controleur.moteur1.reculer(vitesse = 40)  # Recule légèrement pour compléter le demi-tour
             self.controleur.moteur2.reculer(vitesse = 40) 
-            time.sleep(2)
+            time.sleep(1)
             self.controleur.moteur1.arreter()  # Arrête les moteurs
             self.controleur.moteur2.arreter()
             time.sleep(1)
             self.controleur.servo.positionner(angle_brut = 90)  # Recentrer le servo
             time.sleep(1)
 
-            """Manoeuvre 2"""
-            self.controleur.servo.positionner(angle_brut = 45)  # Tourne à gauche de 45 degrés
-            time.sleep(2)  
+            """Manœuvre 3"""
             self.controleur.moteur1.avancer(vitesse = 40)  # Avance légèrement pour compléter le demi-tour
             self.controleur.moteur2.avancer(vitesse = 40)
-            time.sleep(2)  
+            time.sleep(1)
             self.controleur.moteur1.arreter()  # Arrête les moteurs
             self.controleur.moteur2.arreter()
             time.sleep(1)
-            self.controleur.servo.positionner(angle_brut = 90)  # Recentrer le servo
-            time.sleep(1)
-            self.controleur.servo.positionner(angle_brut = 135)  # Tourne à droite de 135 degrés
-            time.sleep(2)
-            self.controleur.moteur1.reculer(vitesse = 40)  # Recule légèrement pour compléter le demi-tour
-            self.controleur.moteur2.reculer(vitesse = 40)
-            time.sleep(2)
-            self.controleur.moteur1.arreter()  # Arrête les moteurs
-            self.controleur.moteur2.arreter()
-            time.sleep(1)
-            self.controleur.servo.positionner(angle_brut = 90)  # Recentrer le servo
-            time.sleep(1)
+
+
+            
+            # self.controleur.servo.positionner(angle_brut = 45)  # Tourne à gauche de 45 degrés
+            # time.sleep(1)  
+            # self.controleur.moteur1.avancer(vitesse = 40)  # Avance légèrement pour compléter le demi-tour
+            # self.controleur.moteur2.avancer(vitesse = 40)
+            # time.sleep(1)  
+            # self.controleur.moteur1.arreter()  # Arrête les moteurs
+            # self.controleur.moteur2.arreter()
+            # time.sleep(1)
+            # self.controleur.servo.positionner(angle_brut = 90)  # Recentrer le servo
+            # time.sleep(1)
+            # self.controleur.servo.positionner(angle_brut = 135)  # Tourne à droite de 135 degrés
+            # time.sleep(1)
+            # self.controleur.moteur1.reculer(vitesse = 40)  # Recule légèrement pour compléter le demi-tour
+            # self.controleur.moteur2.reculer(vitesse = 40)
+            # time.sleep(1)
+            # self.controleur.moteur1.arreter()  # Arrête les moteurs
+            # self.controleur.moteur2.arreter()
+            # time.sleep(1)
+            # self.controleur.servo.positionner(angle_brut = 90)  # Recentrer le servo
+            # time.sleep(1)
 
         except KeyboardInterrupt:
             print("\n[*] Arrêt demandé par l'utilisateur")
