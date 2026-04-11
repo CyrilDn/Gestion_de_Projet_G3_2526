a981810b92a6e5429b9ee13131313824e4d8b2b2
SamyDnx <doneux.samy.uni@gmail.com>
2026-04-11 09:52:41 +0200

    fix: valeur tour 8

--- tests/Script_tour_en_8.py
+++ tests/Script_tour_en_8.py
@@ -59,7 +59,7 @@ class Tour8:
 
             # boucle droite
             self.controleur.servo.positionner(angle_brut=135)
-            time.sleep(3.6)  # durée pour boucler complètement à droite
+            time.sleep(3.5)  # durée pour boucler complètement à droite
 
             # retour centre
             self.controleur.servo.positionner(angle_brut=90)
